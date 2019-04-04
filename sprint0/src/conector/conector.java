f13537c4935414ca9609a1e6cf3e634d895c2037
HILARI7 <44732901+HILARI7@users.noreply.github.com>
2019-04-04 14:10:29 -0400

    gladys

--- sprint0/src/conector/conector.java
+++ sprint0/src/conector/conector.java
@@ -2,7 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- * comprovacion de que hace commit en el github
+ * prueva de gladys de git
  */
 
 package conector;
